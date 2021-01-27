91a79866b3ccab2e582d9f2b2ca17b65e7a07104
Lucas Viana (Bau) <contato@lucasviana.dev>
2021-01-27 17:37:51 -0300

    :memo: adds docs to `user_by_username` helper function

--- flaskr/user.py
+++ flaskr/user.py
@@ -1,6 +1,8 @@
 from flaskr.db import get_db
 
 def user_by_username(username):
+    """Helper function that returns a user based in their username. If user not exists will return none"""
+
     db = get_db()
 
     user = db.execute(
